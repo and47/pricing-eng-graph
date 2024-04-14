ea31517fca996770d2f622ee4b32e7db3d750c49
and47 <and47@users.noreply.github.com>
2024-04-14 18:06:00 +0200

    issue with first naive solution

--- construct_run_valuations.py
+++ construct_run_valuations.py
@@ -1,4 +1,4 @@
-
+from portfolio_tool import AssetGraph
 
 un = AssetGraph()
 un.add_component('TECH')
@@ -13,6 +13,28 @@ un.add_component('INDUSTRIALS')
 un.add_component('TECH', 2, 'INDUSTRIALS')
 un.add_component('AUTOS', 3, 'INDUSTRIALS')
 
+# first naive solution produces intermediate result before correct last one
+un.add_component('AAPL2')
+un.add_component('AAPL', 2, 'AAPL2')
+un.add_component('AAPL4')
+un.add_component('AAPL', 4, 'AAPL4')
+un.add_component('AAPL16')
+un.add_component('AAPL2', 4, 'AAPL16')
+un.add_component('AAPL4', 2, 'AAPL16')
+
+un.merged_view()
+
+un.fix_structure()
+un.init_components()
+
+un.stocks['AAPL'].update_value(173)
+un.stocks['AAPL'].update_value(174)  # prints AAPL16 twice, only 2nd is correct
+
+# need topo-sort or bfs logic not my dfs (with early stop) is still doing 1 redundant step/attempt
+# but topo-sort is expensive to recompute, so cache it?
+# also, reduce by excluding inactive nodes, e.g. if MSFT=nan, do not attempty value TECH
+# in a way, after pricing MSFT, add TECH as "viable to value" -- add to graph?
+# if old price was nan -- enable node
 
 un.merged_view()
 
